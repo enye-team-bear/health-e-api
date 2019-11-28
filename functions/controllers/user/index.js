26f1b451c20d0aface72ed441c773866e5d3d382
sapeled3 <oyinkuromosesvictor@outlook.com>
2019-11-28 21:36:10 +0100

    fix import for login

--- functions/controllers/user/index.js
+++ functions/controllers/user/index.js
@@ -2,13 +2,13 @@ const config = require("../../util/config");
 const firebase = require("firebase");
 require("firebase/firestore");
 firebase.initializeApp(config);
-const signUp = require('./signUp');
-const login = require('./login')
+const { signupUser }= require('./signUp');
+const { loginUser } = require('./login')
 
 const db = firebase.firestore();
 
-const loginUser = (req, res) => login.loginUser(req, res, db );
-const signUpUser = (req, res) => signUp.signupUser(req, res, db);
+const loginUser = (req, res) => loginUser(req, res, db );
+const signUpUser = (req, res) => signupUser(req, res, db);
 
 module.exports = {
     loginUser,
