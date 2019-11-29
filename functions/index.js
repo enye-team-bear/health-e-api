8e3aef622a8ae29548187aaf253e72f9fff86191
Sapele Moses <46188071+SapeleD3@users.noreply.github.com>
2019-11-29 16:59:35 +0100

    Revert "[add] signup endpoint"

--- functions/index.js
+++ functions/index.js
@@ -6,11 +6,5 @@ const app = express()
 
 app.use(cors())
 
-const { loginUser, signUpUser } = require('./controllers/user/index')
-
-//user Routes
-app.post('/signup', signUpUser);
-app.post('/login', loginUser);
-
 exports.api = functions.https.onRequest(app);
 
