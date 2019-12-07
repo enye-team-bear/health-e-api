52c7f1a991fe6674863c8ab62450540d0e566985
sapeled3 <oyinkuromosesvictor@outlook.com>
2019-12-07 19:22:27 +0100

    [add] get user by username

---
+++ functions/controllers/user/getAUser.js
@@ -0,0 +1,40 @@
+const _ = require('lodash');
+const HttpStatus = require('http-status-codes');
+const { message, status } = require('../../util/constants');
+
+const { BAD_REQUEST, OK, INTERNAL_SERVER_ERROR } = HttpStatus;
+const { error, success } = status;
+const { somethingWentWrong } = message;
+
+const mapUsers = async (res, users) => {
+    const user = users.data();
+    return res.status(OK).json({ data: user, status: success });
+};
+
+const getUser = async (req, res, db) => {
+    const userName = req.params.userName.toLowerCase();
+    const users = await db
+        .collection('users')
+        .doc(userName)
+        .get();
+    if (!users) {
+        return res
+            .status(BAD_REQUEST)
+            .json({ message: somethingWentWrong, status: error });
+    }
+    return mapUsers(res, users);
+};
+
+const getAUser = async (req, res, db) => {
+    try {
+        return getUser(req, res, db);
+    } catch (err) {
+        return res
+            .status(INTERNAL_SERVER_ERROR)
+            .json({ message: somethingWentWrong, status: error });
+    }
+};
+
+module.exports = {
+    getAUser,
+};
--- functions/controllers/user/index.js
+++ functions/controllers/user/index.js
@@ -3,14 +3,17 @@ const { signupUser } = require('./signUp');
 const { loginUser } = require('./login');
 const { imageUpload } = require('./imageUpload');
 const { getAllUser } = require('./getUsers');
+const { getAUser } = require('./getAUser');
 
 const login = (req, res) => loginUser(req, res, db);
 const signUp = (req, res) => signupUser(req, res, db);
 const image = (req, res) => imageUpload(req, res, db);
 const getAll = (req, res) => getAllUser(req, res, db);
+const getUserById = (req, res) => getAUser(req, res, db);
 
 module.exports = {
     getAll,
+    getUserById,
     image,
     login,
     signUp,
