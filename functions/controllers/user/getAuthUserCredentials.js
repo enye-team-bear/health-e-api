1edfc766c9d92d95218ab5dd61d69a622ec2da67
sapeled3 <oyinkuromosesvictor@outlook.com>
2019-12-15 10:50:39 +0100

    [fix] http status code import

--- functions/controllers/user/getAuthUserCredentials.js
+++ functions/controllers/user/getAuthUserCredentials.js
@@ -1,39 +1,38 @@
-const HttpStatus = require('http-status-codes');
+const { BAD_REQUEST, OK, INTERNAL_SERVER_ERROR } = require('http-status-codes');
 const { message, status } = require('../../util/constants');
 
-const { BAD_REQUEST, OK, INTERNAL_SERVER_ERROR } = HttpStatus;
 const { error, success } = status;
 const { somethingWentWrong } = message;
 
 const mapUsers = async (res, users) => {
-    const user = users.data();
-    return res.status(OK).json({ data: user, status: success });
+	const user = users.data();
+	return res.status(OK).json({ data: user, status: success });
 };
 
 const getUser = async (req, res, db) => {
-    const userName = req.user.userName.toLowerCase();
-    const users = await db
-        .collection('users')
-        .doc(userName)
-        .get();
-    if (!users) {
-        return res
-            .status(BAD_REQUEST)
-            .json({ message: somethingWentWrong, status: error });
-    }
-    return mapUsers(res, users);
+	const userName = req.user.userName.toLowerCase();
+	const users = await db
+		.collection('users')
+		.doc(userName)
+		.get();
+	if (!users) {
+		return res
+			.status(BAD_REQUEST)
+			.json({ message: somethingWentWrong, status: error });
+	}
+	return mapUsers(res, users);
 };
 
 const getAuthUserCredentials = async (req, res, db) => {
-    try {
-        return getUser(req, res, db);
-    } catch (errors) {
-        return res
-            .status(INTERNAL_SERVER_ERROR)
-            .json({ message: somethingWentWrong, status: errors });
-    }
+	try {
+		return getUser(req, res, db);
+	} catch (errors) {
+		return res
+			.status(INTERNAL_SERVER_ERROR)
+			.json({ message: somethingWentWrong, status: errors });
+	}
 };
 
 module.exports = {
-    getAuthUserCredentials,
+	getAuthUserCredentials,
 };
