3230ed5dc1d4a54f859369bfed0b11c5e051d947
Retzam Tarle <Younggregs>
2019-12-17 13:48:13 +0100

    fixed conflicts 2

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -4,7 +4,12 @@ const router = express.Router();
 const authGuard = require('../util/authGuard');
 
 const {
-    login, signUp, image, getAll,
+    login,
+    signUp,
+    image,
+    getAll,
+    getUserById,
+    AuthUserCredentials,
 } = require('../controllers/user/index');
 
 const {
@@ -20,6 +25,8 @@ router.post('/signup', signUp);
 router.post('/login', login);
 router.put('/user/image', authGuard, image);
 router.get('/users', getAll);
+router.get('/user', authGuard, AuthUserCredentials);
+router.get('/user/:userName', getUserById);
 router.post('/new_topic', newTopic);
 router.get('/get_topics', getTopics);
 router.post('/new_post', newPost);
