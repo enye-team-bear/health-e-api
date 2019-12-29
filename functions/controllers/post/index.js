01f338181c1676548d95c3d102c6eaaf2fd0d6d9
sapeled3 <oyinkuromosesvictor@outlook.com>
2019-12-29 16:20:30 +0100

    [add] get post by id

--- functions/controllers/post/index.js
+++ functions/controllers/post/index.js
@@ -1,21 +1,23 @@
-/* eslint-disable no-tabs */
-/* eslint-disable indent */
 const { db } = require('../../util/admin');
 const { addPost } = require('./newPost');
 const { getAllPosts } = require('./getPost');
 const { addComments } = require('./addComment');
 const { likePost } = require('./likePost');
 const { unlikePost } = require('./unlikePost');
+const { getPostById } = require('./getPostById');
 
 const getPosts = (req, res) => getAllPosts(req, res, db);
 const newPost = (req, res) => addPost(req, res, db);
 const comment = (req, res) => addComments(req, res, db);
 const like = (req, res) => likePost(req, res, db);
 const unlike = (req, res) => unlikePost(req, res, db);
+const getPostId = (req, res) => getPostById(req, res, db);
+
 module.exports = {
-	comment,
-	getPosts,
-	like,
-	newPost,
-	unlike,
+    comment,
+    getPostId,
+    getPosts,
+    like,
+    newPost,
+    unlike,
 };
--- functions/routes/index.js
+++ functions/routes/index.js
@@ -1,33 +1,32 @@
-/* eslint-disable indent */
-/* eslint-disable no-tabs */
 const express = require('express');
 
 const router = express.Router();
 const authGuard = require('../util/authGuard');
 
 const {
-	login,
-	signUp,
-	image,
-	getAll,
-	getUserById,
-	AuthUserCredentials,
+    login,
+    signUp,
+    image,
+    getAll,
+    getUserById,
+    AuthUserCredentials,
 } = require('../controllers/user/index');
 
 const {
-	newTopic,
-	getTopics,
-	commentOnTopic,
-	likeTop,
-	unlikeTop,
+    newTopic,
+    getTopics,
+    commentOnTopic,
+    likeTop,
+    unlikeTop,
 } = require('../controllers/topic/index');
 
 const {
-	newPost,
-	getPosts,
-	comment,
-	like,
-	unlike,
+    newPost,
+    getPostId,
+    getPosts,
+    comment,
+    like,
+    unlike,
 } = require('../controllers/post/index');
 
 // user Routes
@@ -43,6 +42,7 @@ router.post('/topic/:topicId/comment', authGuard, commentOnTopic);
 router.get('/topic/:topicId/like', authGuard, likeTop);
 router.get('/topic/:topicId/unlike', authGuard, unlikeTop);
 router.post('/post/:postId/comment', authGuard, comment);
+router.get('/post/:postId', getPostId);
 router.get('/post/:postId/like', authGuard, like);
 router.get('/post/:postId/unlike', authGuard, unlike);
 router.post('/new_post', authGuard, newPost);
